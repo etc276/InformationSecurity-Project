970e866da1c4bb8266027919ca1dd4aeb0158b08
洪得揚 <etc276@gmail.com>
2016-11-25 18:21:33 +0800

    client and server for cryp
    5 mode of DES

---
+++ CFB/Server.java
@@ -0,0 +1,49 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.crypto.*;
+import java.security.*;
+import sun.misc.*;
+import javax.crypto.spec.*;
+
+public class Server {
+    public static int port = 20;
+ 
+    public static void main(String args[]) throws Exception {
+        ServerSocket ss = new ServerSocket(port);
+        while (true) {
+            Socket server = ss.accept();
+            System.out.println("Someone connected.");
+
+            // use baos to read byte array , cause InputStream.read() can read 1byte/times
+            InputStream input = server.getInputStream();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte [] buffer = new byte[512];
+            int length = 0;     // record length to prevent unnecessary data
+            while ( (length = input.read(buffer)) !=-1)     // if length = -1 means no data
+                baos.write(buffer, 0, length);
+            byte data [] = baos.toByteArray();
+
+            byte [] iv = {0,0,0,0,0,0,0,0};
+            IvParameterSpec ivSpec = new IvParameterSpec(iv);
+
+            // usable key (spec, factory and secret)
+            String key = "password";
+            DESKeySpec keySpec = new DESKeySpec(key.getBytes("UTF-8"));
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey secretKey = keyFactory.generateSecret(keySpec);
+
+            // decrypt
+            Cipher cipher = Cipher.getInstance("DES/CFB/NoPadding");
+            cipher.init(Cipher.DECRYPT_MODE , secretKey , ivSpec);
+            byte[] plain = cipher.doFinal(data);
+            System.out.println(new String(plain));
+
+            // close stream and socket
+            input.close();
+            server.close();
+        }
+    }
+}
---
+++ CTR/Client.java
@@ -0,0 +1,61 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import javax.crypto.*;
+import java.security.*;
+import sun.misc.*;
+import javax.crypto.spec.*;
+
+public class Client {
+    public static int port = 20;
+    
+    public static void main(String args[]) throws Exception {
+
+        // plainText, key, iv, and random 
+        String plainText = "Hello, CTR test.";
+        String key = "password";
+        byte [] iv = {0,0,0,0,1,1,1,1};
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+        SecureRandom random = new SecureRandom(); 
+
+        // usable key (spec, factory and secret)
+        DESKeySpec keySpec = new DESKeySpec(key.getBytes());
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+        SecretKey secretKey = keyFactory.generateSecret(keySpec);
+
+        // encrypt
+        Cipher cipher = Cipher.getInstance("DES/CTR/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey , ivSpec);
+        byte[] cipherData = cipher.doFinal(plainText.getBytes("UTF-8"));
+
+        // connect server
+        Socket client = new Socket("127.0.0.1", port);
+
+        // print to screen
+        System.out.println("...CTR...");
+        System.out.println("PlainText :" + plainText);
+        System.out.println("CipherText:" + cipherData);
+
+        // write outputstream
+        OutputStream out = client.getOutputStream();
+        out.write(cipherData);
+
+        // end of connect, close stream and class
+        out.close();
+        client.close();
+
+        /*
+        InputStream in = client.getInputStream();
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(in)
+        );
+        
+        String msg = reader.readLine();
+        System.out.println(msg);
+        client.close();
+        */
+    }
+}
